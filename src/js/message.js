50d007b341eb42e8c7420968853b1436b7a5e797
zhaoyang <18355403288@163.com>
2019-05-17 14:34:13 +0800

    update

--- src/js/message.js
+++ src/js/message.js
@@ -1,25 +1,64 @@
 var vm = new Vue({
   el: '#app',
   data: {
+    commentMsg: '',
     commentList: [
-      {
-        floor: 2,
-        author: 'author',
-        detail: 'flofloorfloor detail',
-      }, {
-        floor: 3,
-        author: 'author',
-        detail: 'flofloorfloor detail',
-      }, {
-        floor: 2,
-        author: 'author',
-        detail: 'flofloorfloor detail',
-      }
+      // {
+      //   floor: 2,
+      //   author: 'author',
+      //   comment: 'flofloorfloor comment',
+      //   date: '18'
+      // },
+      // {
+      //   floor: 3,
+      //   author: 'author',
+      //   comment: 'flofloorfloor comment',
+      //   date: '18'
+      // },
+      // {
+      //   floor: 2,
+      //   author: 'author',
+      //   comment: 'flofloorfloor comment',
+      //   date: '18'
+      // }
     ]
+  },
+  created: function() {
+    this.initData();
+  },
+  methods: {
+    sendMsg: function(){
+      let params = {
+        author: 'TEST',
+        comment: this.commentMsg
+      };
+      ajax(
+        'post',
+        '/cv/add',
+        params,
+        function(res) {
+          window.location.reload();
+        },
+        function() {}
+      );
+    },
+    initData: function() {
+      let that = this;
+      ajax(
+        'get',
+        '/cv/query',
+        {},
+        function(res) {
+          let commentList = res;
+          commentList = JSON.parse(commentList);
+          commentList.forEach((item, index) => {
+            item.floor = index;
+            item.date = 18;
+          });
+          that.commentList = commentList;
+        },
+        function() {}
+      );
+    }
   }
 });
-
-ajax('post', '/index.php/pay/api/api/h5', {}, function(res){
-  let data = res.data;
-
-}, function(){});
