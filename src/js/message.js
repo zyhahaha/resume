13e4f2e901cf488d2bbadafb5fabc24a2739a493
zhaoyang <18355403288@163.com>
2019-05-17 15:30:09 +0800

    update

--- src/js/message.js
+++ src/js/message.js
@@ -53,12 +53,36 @@ var vm = new Vue({
           commentList = JSON.parse(commentList);
           commentList.forEach((item, index) => {
             item.floor = index;
-            item.date = 18;
+            item.date = that.timeElapse(item.tim);
           });
           that.commentList = commentList;
+          console.log(commentList);
         },
         function() {}
       );
+    },
+    timeElapse: function(dateTim){
+      var current = Date();
+      var seconds = (Date.parse(current) - dateTim) / 1000;
+      var days = Math.floor(seconds / (3600 * 24));
+      seconds = seconds % (3600 * 24);
+      var hours = Math.floor(seconds / 3600);
+      seconds = seconds % 3600;
+      var minutes = Math.floor(seconds / 60);
+      seconds = seconds % 60;
+      seconds = Math.floor(seconds);
+      
+      var result = '';
+      if (days) {
+        result = days + ' 天前';
+      } else if(hours) {
+        result = hours + ' 小时前';
+      } else if(minutes) {
+        result = minutes + ' 分钟前';
+      } else if(seconds) {
+        result = seconds + ' 秒前';
+      }
+      return result;
     }
   }
 });
